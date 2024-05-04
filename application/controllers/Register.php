06778b5ecdc2732ae12cd3fbe8ce88b7b3fae1a6
Ruth Cedeño <emperatrizcedeno18@gmail.com>
2024-05-04 11:07:27 -0400

    migrations, models, register and validations

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -8,6 +8,7 @@ class Register extends CI_Controller
         parent::__construct();
         $this->load->helper(array('get_menu'));
         $this->load->model('Users');
+        $this->load->library(array('form_validation'));
     }
 
     public function index()
@@ -19,6 +20,8 @@ class Register extends CI_Controller
 
     public function create()
     {
+        //data extracted from form
+
         $data = array(
             'name' => $this->input->post('name'),
             'lastname' => $this->input->post('lastname'),
@@ -26,13 +29,68 @@ class Register extends CI_Controller
             'password' => $this->input->post('password'),
         );
 
-        $home['home'] = home_url();
+        //validations
 
-        if (!$this->Users->create($data)) {
-            $home['msg'] = 'error to register';
-            $this->load->view('register', $data);
+        $config = array(
+            array(
+                'field' => 'name',
+                'label' => 'Name',
+                'rules' => 'required|alpha',
+                'errors' => array(
+                    'required' => 'You must provide a %s.',
+                ),
+            ),
+            array(
+                'field' => 'lastname',
+                'label' => 'Last Name',
+                'rules' => 'required|alpha',
+                'errors' => array(
+                    'required' => 'You must provide a %s.',
+                ),
+
+            ),
+            array(
+                'field' => 'email',
+                'label' => 'Email',
+                'rules' => 'required|valid_email',
+                'errors' => array(
+                    'required' => 'You must provide a %s.',
+                ),
+            ),
+            array(
+                'field' => 'password',
+                'label' => 'Password',
+                'rules' => 'required|min_length[8]',
+                'errors' => array(
+                    'required' => 'You must provide a %s.',
+                ),
+            ),
+            array(
+                'field' => 'password-confirm',
+                'label' => 'PasswordConfirmation',
+                'rules' => 'required|min_length[8]',
+                'errors' => array(
+                    'required' => 'You must provide a %s.',
+                ),
+            ),
+        );
+
+        $this->form_validation->set_rules($config);
+
+        if ($this->form_validation->run() == FALSE) {
+            $home['home'] = home_url();
+            $this->load->view('register', $home);
+        } else {
+            $home['home'] = home_url();
+
+            //send data to model and create user
+
+            if (!$this->Users->create($data)) {
+                $home['msg'] = 'error to register';
+                $this->load->view('register', $data);
+            }
+            $home['msg'] = 'register sucessfully';
+            $this->load->view('register', $home);
         }
-        $home['msg'] = 'register sucessfully';
-        $this->load->view('register', $home);
     }
 }
